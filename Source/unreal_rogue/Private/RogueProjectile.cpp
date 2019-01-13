bb58321d8105a294b044e413ef194fc5a5b6befa
jolampi <36279434+jolampi@users.noreply.github.com>
2019-01-13 23:54:33 +0200

    Nwm it did work. Thanks Unreal. Cleaning up etc

--- Source/unreal_rogue/Private/RogueProjectile.cpp
+++ Source/unreal_rogue/Private/RogueProjectile.cpp
@@ -8,13 +8,9 @@ ARogueProjectile::ARogueProjectile() {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Use a sphere as a simple collision representation.
+	// Use sphere for collision
 	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
-	// Set the sphere's collision radius.
 	CollisionComponent->InitSphereRadius(15.0f);
-	CollisionComponent->SetSimulatePhysics(true);
-	CollisionComponent->SetNotifyRigidBodyCollision(true);
-	//CollisionComponent->BodyInstance.SetCollisionProfileName("PlayerProjectile"); <-- this was useless with overlay, probably here also
 	CollisionComponent->OnComponentHit.AddDynamic(this, &ARogueProjectile::OnComponentHit);
 
 	// Set the root component to be the collision component.
@@ -25,7 +21,6 @@ ARogueProjectile::ARogueProjectile() {
 	TriggerComponent->InitSphereRadius(15.0f);
 	TriggerComponent->SetupAttachment(RootComponent);
 	TriggerComponent->OnComponentBeginOverlap.AddDynamic(this, &ARogueProjectile::OnOverlapBegin);
-	
 
 	// Use this component to drive this projectile's movement.
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
@@ -59,7 +54,7 @@ void ARogueProjectile::FireInDirection(const FVector& ShootDirection) {
 void ARogueProjectile::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
 	if (OtherActor && (OtherActor != this) && OtherComp) {
 		if (GEngine) {
-			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Projectile destroyed"));
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Orange, TEXT("Bullet Overlay"));
 		}
 		Destroy();
 	}
@@ -68,7 +63,7 @@ void ARogueProjectile::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp,
 void ARogueProjectile::OnComponentHit(class UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) {
 	if (OtherActor && (OtherActor != this) && OtherComp) {
 		if (GEngine) {
-			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("HIT"));
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Orange, TEXT("Bullet collide"));
 		}
 		Destroy();
 	}
